11f8ea910497da4704a1d2628bc8f644b8b1fd46
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-03-21 15:12:40 -0700

    add rx timer code

--- goatstone/notes/note.js
+++ goatstone/notes/note.js
@@ -1,4 +1,16 @@
+'use strict'
+const Rx = require('rx')
 
-var e = require('Events')
- 
-console.log('note' , e)
+const cities = ['new-york', 'paris', 'rome', 'london']
+const delay = 100
+const period = 1000
+const cityStream = Rx.Observable.timer( delay, period )
+    .map( i => cities[ i ] )
+    .take( cities.length )
+
+    cityStream
+    .subscribe(
+    (x) => {
+        console.log('Next: ' + x)
+    },
+    err => { throw err }, () => console.log('Completed') )

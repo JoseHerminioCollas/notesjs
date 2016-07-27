999bf96eea2241a6c33d672224f32870d44c4f07
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-07-27 10:44:33 -0700

    tree walking

---
+++ goatstone/notes/walkit.js
@@ -0,0 +1,36 @@
+function Node(d) {
+    this.l
+    this.r
+    this.d = d
+}
+var a = new Node('8')
+a.l = new Node('5')
+a.r = new Node('4')
+a.r.r = new Node('11')
+a.r.r.l = new Node('3')
+
+a.l.l = new Node('9')
+
+a.l.r = new Node('7')
+a.l.r.l = new Node('1')
+a.l.r.r = new Node('12')
+
+a.l.r.r.l = new Node('2')
+
+var stck = []
+walkIt(a)
+function walkIt(el) {
+    stck.push(el)
+    while (stck.length > 0) {
+        let curr = stck.pop()
+        console.log('curr', curr.d)
+        if (curr.r) {
+            stck.push(curr.r)
+
+        }
+        if (curr.l) {
+            stck.push(curr.l)
+        }
+    }
+
+}

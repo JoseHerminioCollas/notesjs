6503cc15d39e1592d77129c02ea582e758bb80b0
Jose Collas <joseherminiocollas@goatstone.com>
2016-12-10 09:55:03 -0800

    Replace Action bar with Toolbar.

--- goatstone/ui/material-app-demo.js
+++ goatstone/ui/material-app-demo.js
@@ -5,16 +5,34 @@ import ActionHome from 'material-ui/svg-icons/action/home'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import {MenuItem} from 'material-ui/Menu'
 import AppBar from 'material-ui/AppBar'
+import {List, ListItem} from 'material-ui/List'
+import ActionGrade from 'material-ui/svg-icons/action/grade'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
+import Divider from 'material-ui/Divider'
 
 const App = (props) => (
   <div>
-    <AppBar
-      title="Make A List"
-      iconElementLeft={
-        <ActionHome style={{marginTop: '12px', color: '#ccc'}} />
-      }
-      iconElementRight={
-        <IconMenu
+  <Toolbar>
+    <ToolbarGroup firstChild={true}>
+      <ToolbarTitle text="Make A List" style={{marginLeft: '20px'}} />
+    </ToolbarGroup>
+    <ToolbarGroup>
+        <FloatingActionButton style={{marginRight: '20px'}} mini={true}
+            onTouchTap={
+                e => {
+                    e.preventDefault() // This prevents ghost click.
+                    console.log('action!!!')
+                    props.eventEmitter.emit('popover',
+                    {show: true, target: 'popover', content: 'about'})
+                }
+            }
+            >
+            <ContentAdd />
+        </FloatingActionButton>
+        <Divider />
+         <IconMenu
               iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
               anchorOrigin={{horizontal: 'left', vertical: 'top'}}
               targetOrigin={{horizontal: 'left', vertical: 'top'}}>
@@ -37,8 +55,19 @@ const App = (props) => (
                 }
               />
           </IconMenu>
-        }
-    />
+
+        </ToolbarGroup>
+    </Toolbar>
+    <div id="d" data-target="popover"></div>
+    <List>
+      <ListItem primaryText="a list item" />
+      <ListItem primaryText="abc def" leftIcon={<ActionGrade />} />
+      <ListItem primaryText="123" />
+      <ListItem primaryText="A" />
+      <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+      <ListItem primaryText="B" />
+      <ListItem primaryText="C" />
+    </List>
   </div>
 )
 export default App

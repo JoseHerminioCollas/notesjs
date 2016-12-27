9b78dfd98d468f1a97ac2db5136542f22c474635
Jose Collas <joseherminiocollas@goatstone.com>
2016-12-27 10:29:16 -0800

    Use Class for main Application component.

--- goatstone/react-style-rx.js
+++ goatstone/react-style-rx.js
@@ -5,22 +5,23 @@ import events from 'events'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 // import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 // import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
+// import injectTapEventPlugin from 'react-tap-event-plugin'
+// injectTapEventPlugin()
 import App from 'goatstone/ui/material-app-demo'
 const log = require('goatstone/log/log.js')
 // import RenderMessage from 'goatstone/ui/render-message'
-import RenderPO from 'goatstone/ui/popover.js'
-import RenderSnackbar from 'goatstone/ui/render-snackbar.js'
+// import RenderPO from 'goatstone/ui/popover.js'
+// import RenderSnackbar from 'goatstone/ui/render-snackbar.js'
 const eventEmitter = new events.EventEmitter()
 const messageStream = Rx.Observable.fromEvent(eventEmitter, 'message')
 // RenderMessage('#message', messageStream)
-const popoverStream = Rx.Observable.fromEvent(eventEmitter, 'popover')
-RenderPO('#d', popoverStream, eventEmitter)
+// const popoverStream = Rx.Observable.fromEvent(eventEmitter, 'popover')
+// RenderPO('#d', popoverStream, eventEmitter)
 
 const logStream = Rx.Observable.merge(
-  messageStream,
-  popoverStream
+  messageStream
+  // ,
+  // popoverStream
 )
 logStream.subscribe(
   x => {
@@ -29,17 +30,17 @@ logStream.subscribe(
   err => log('e', err),
   () => log('c'))
 
-RenderSnackbar('#snackbar', logStream)
+// RenderSnackbar('#snackbar', logStream)
 
 // render the React application   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-ReactDOM.render(
-  <MuiThemeProvider>
-  <App
-    eventEmitter={eventEmitter}
-  />
-  </MuiThemeProvider>,
-  document.getElementById('app')
-)
+// ReactDOM.render(
+//   <MuiThemeProvider>
+//   <App
+//     eventEmitter={eventEmitter}
+//   />
+//   </MuiThemeProvider>,
+//   document.getElementById('app')
+// )
 
 // emit the initial events
-eventEmitter.emit('message', {action: 'show', target: 'message', message: 'Welcome to Make A List'})
+// eventEmitter.emit('message', {action: 'show', target: 'message', message: 'Welcome to Make A List'})
--- goatstone/ui/material-app-demo.js
+++ goatstone/ui/material-app-demo.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import Rx from 'rx'
+import ReactDOM from 'react-dom'
+const log = require('goatstone/log/log.js')
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 // import ActionHome from 'material-ui/svg-icons/action/home'
@@ -11,63 +18,175 @@ import FloatingActionButton from 'material-ui/FloatingActionButton'
 import ContentAdd from 'material-ui/svg-icons/content/add'
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 import Divider from 'material-ui/Divider'
+import Snackbar from 'material-ui/Snackbar'
 
-const App = (props) => (
-  <div>
-  <Toolbar>
-    <ToolbarGroup firstChild={true}>
-      <ToolbarTitle text="Make A List" style={{marginLeft: '20px'}} />
-    </ToolbarGroup>
-    <ToolbarGroup>
-        <FloatingActionButton style={{marginRight: '20px'}} mini={true}
-            onTouchTap={
-                e => {
-                    e.preventDefault() // This prevents ghost click.
-                    console.log('action!!!')
-                    props.eventEmitter.emit('popover',
-                    {show: true, target: 'popover', content: 'about'})
-                }
-            }
-            >
-            <ContentAdd />
-        </FloatingActionButton>
-        <Divider />
-         <IconMenu
-              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-              anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-              targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-              <MenuItem primaryText="About"
-                  onTouchTap={
-                      e => {
-                          e.preventDefault() // This prevents ghost click.
-                          props.eventEmitter.emit('popover',
-                          {show: true, target: 'popover', content: 'about'})
-                      }
-                  }
-              />
-              <MenuItem primaryText="Settings"
-                onTouchTap={
-                    e => {
-                        e.preventDefault() // This prevents ghost click.
-                        props.eventEmitter.emit('popover',
-                        {show: true, target: 'popover', content: 'settings'})
+import events from 'events'
+const eventEmitter = new events.EventEmitter()
+const popoverStream = Rx.Observable.fromEvent(eventEmitter, 'popover')
+
+class A extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            a: '1111 xx',
+            date: new Date(),
+            isOpenSnackBar: true,
+            msg: 'abc'
+        }
+    }
+    componentDidMount () {
+        popoverStream.subscribe(x => {
+            this.setState({
+                isOpenSnackBar: true,
+                msg: 'hello'
+            })
+            console.log('44', this)
+        }, err => log('e', err), () => log('c'))
+    }
+    render () {
+        return (
+          <MuiThemeProvider>
+          <div>
+
+          <Toolbar>
+            <ToolbarGroup firstChild={true}>
+              <ToolbarTitle text="Make A List" style={{marginLeft: '20px'}} />
+            </ToolbarGroup>
+            <ToolbarGroup>
+                <FloatingActionButton style={{marginRight: '20px'}} mini={true}
+                    onTouchTap={
+                        e => {
+                            e.preventDefault() // This prevents ghost click.
+                            console.log('action!!!')
+                            eventEmitter.emit('popover',
+                            {show: true, target: 'popover', content: 'about'})
+                        }
                     }
-                }
-              />
-          </IconMenu>
-
-        </ToolbarGroup>
-    </Toolbar>
-    <div id="d" data-target="popover"></div>
-    <List>
-      <ListItem primaryText="a list item" />
-      <ListItem primaryText="abc def" leftIcon={<ActionGrade />} />
-      <ListItem primaryText="123" />
-      <ListItem primaryText="A" />
-      <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
-      <ListItem primaryText="B" />
-      <ListItem primaryText="C" />
-    </List>
-  </div>
+                    >
+                    <ContentAdd />
+                </FloatingActionButton>
+                <Divider />
+                 <IconMenu
+                      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                      anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                      targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+                      <MenuItem primaryText="About"
+                          onTouchTap={
+                              e => {
+                                  e.preventDefault() // This prevents ghost click.
+                                  eventEmitter.emit('popover',
+                                  {show: true, target: 'popover', content: 'about'})
+                              }
+                          }
+                      />
+                      <MenuItem primaryText="Settings"
+                        onTouchTap={
+                            e => {
+                                e.preventDefault() // This prevents ghost click.
+                                eventEmitter.emit('popover',
+                                {show: true, target: 'popover', content: 'settings'})
+                            }
+                        }
+                      />
+                  </IconMenu>
+                </ToolbarGroup>
+            </Toolbar>
+            <div id="d" data-target="popover"></div>
+            <List>
+              <ListItem primaryText="a list item" />
+              <ListItem primaryText="abc def" leftIcon={<ActionGrade />} />
+              <ListItem primaryText="123" />
+              <ListItem primaryText="A" />
+              <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+              <ListItem primaryText="B" />
+              <ListItem primaryText="C" />
+            </List>
+            <Snackbar
+              open={this.state.isOpenSnackBar}
+              message={this.state.msg}
+              autoHideDuration={3000}
+              onRequestClose={x => {
+                  this.setState({
+                      isOpenSnackBar: false
+                  })
+              }}
+            />
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            <h2>It is {this.state.a}.</h2>
+          </div>
+          </MuiThemeProvider>
+        )
+    }
+}
+ReactDOM.render(
+    <A />,
+  document.getElementById('app')
 )
+// ReactDOM.render(
+//   <MuiThemeProvider>
+//   <App
+//     eventEmitter={eventEmitter}
+//   />
+//   </MuiThemeProvider>,
+//   document.getElementById('app')
+// )
+
+// const App = (props) => (
+//   <div>
+//   <Toolbar>
+//     <ToolbarGroup firstChild={true}>
+//       <ToolbarTitle text="Make A List" style={{marginLeft: '20px'}} />
+//     </ToolbarGroup>
+//     <ToolbarGroup>
+//         <FloatingActionButton style={{marginRight: '20px'}} mini={true}
+//             onTouchTap={
+//                 e => {
+//                     e.preventDefault() // This prevents ghost click.
+//                     console.log('action!!!')
+//                     eventEmitter.emit('popover',
+//                     {show: true, target: 'popover', content: 'about'})
+//                 }
+//             }
+//             >
+//             <ContentAdd />
+//         </FloatingActionButton>
+//         <Divider />
+//          <IconMenu
+//               iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+//               anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+//               targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+//               <MenuItem primaryText="About"
+//                   onTouchTap={
+//                       e => {
+//                           e.preventDefault() // This prevents ghost click.
+//                           eventEmitter.emit('popover',
+//                           {show: true, target: 'popover', content: 'about'})
+//                       }
+//                   }
+//               />
+//               <MenuItem primaryText="Settings"
+//                 onTouchTap={
+//                     e => {
+//                         e.preventDefault() // This prevents ghost click.
+//                         eventEmitter.emit('popover',
+//                         {show: true, target: 'popover', content: 'settings'})
+//                     }
+//                 }
+//               />
+//           </IconMenu>
+//
+//         </ToolbarGroup>
+//     </Toolbar>
+//     <div id="d" data-target="popover"></div>
+//     <List>
+//       <ListItem primaryText="a list item" />
+//       <ListItem primaryText="abc def" leftIcon={<ActionGrade />} />
+//       <ListItem primaryText="123" />
+//       <ListItem primaryText="A" />
+//       <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+//       <ListItem primaryText="B" />
+//       <ListItem primaryText="C" />
+//     </List>
+//   </div>
+// )
 export default App

152e54fe4866c252102946d1a0792db37c04b3ee
Jose Collas <joseherminiocollas@goatstone.com>
2017-09-01 09:18:13 -0700

    add file

---
+++ goatstone/notes/rev-ll.js
@@ -0,0 +1,21 @@
+let hd = {d: 2, n: {d: 4, n: {d: 9, n: null}}} 
+
+function rll(h) {
+    let c = h
+    let p = null
+    let n = null
+    while (c) {
+        n = c.n // set the next
+        c.n = p
+        p = c
+        c = n
+        console.log(c)
+    }
+    h = p
+    return h
+}
+
+console.log('xxx', rll(hd) )
+
+function Note () { return 1 }
+module.exports = Note

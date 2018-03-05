d103ae1e7a3fb62abc13a9066cefa9ca22bda1cb
Jose Collas <joseherminiocollas@goatstone.com>
2018-03-05 08:52:28 -0800

    refactor for run time

--- test/note.test.js
+++ test/note.test.js
@@ -1,43 +1,9 @@
 var expect = require('chai').expect
-const Canvas = require('goatstone/notes/note.js')
+const Note = require('goatstone/notes/note')
 
-describe('Simple Draw', function () {
-    describe('vertical line', () => {
-        const t = new Canvas(3, 5)
-              .draw(2, 0, 2, 2)
-              .drawCanvas()
-        it('should draw a vertical line', () => {
-            expect(1).to.equal(1)
-        })
-    })
-    describe('horizontal line', () => {
-        before(() => {
-            const s = new Canvas(3, 3)
-                  .draw(2, 1, 0, 1)
-                  .drawCanvas()            
-        })
-        it('should draw a horizontal line', () => {
-            expect(1).to.equal(1)
-        })
-    })
-    describe('diagonal line as box', () => {
-        const u = new Canvas(3, 5)
-              .draw(0, 0, 2, 2)
-              .drawCanvas()
-        it('draw a box from to points', () => {
-            expect(1).to.equal(1)
-        })
-    })
-    describe('flood fill', () => {
-        it('should fill rectangle', function() {
-            let c = new Canvas(7, 7)
-            let actual = c
-                .draw(1, 1, 5, 4)
-                .fill(3, 3, 'o')
-//                .fill(0, 0, 'o')
-            const expectedResult = '---------\n|       |\n| xxxxx |\n| xooox |\n| xooox |\n| xxxxx |\n|       |\n|       |\n---------'
-            const result = c.drawCanvas()
-            expect(result).to.equal(expectedResult)
-        })
+describe('Notes Test', function () {
+    it('should....', () => {
+        Note()
+        expect(1).to.equal(1)
     })
 })

47586197dfe6b59f10e747abba61413f76011b6d
goatstone <jose.collas@goatstone.com>
2018-06-28 17:04:20 -0700

    update with Docker file, refactor.

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,25 @@ var BUILD_DIR = path.resolve(__dirname, 'dist/')
 var APP_DIR = path.resolve(__dirname, 'goatstone')
 var mainFileNames = [
     '/array-sort.js',
-    '/note.js',
-    '/react-rx.js',
-    '/react-component-creation.js',
-    '/react-style.js',
-    '/react-style-rx.js'
+    '/notes/note.js'
 ]
 
 var config = {
-    entry: APP_DIR + mainFileNames[5],
+    entry: APP_DIR + mainFileNames[1],
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
-            {
-                test: /\.jsx?/,
-                include: APP_DIR,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }]
+        loaders: [{
+            test: /\.js?/,
+            include: APP_DIR,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
     }
 }
 

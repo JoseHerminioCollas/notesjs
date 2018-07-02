3f89271a4354cfe875c90f0d66e34f25a42d142d
Jose Collas <joseherminiocollas@goatstone.com>
2018-07-02 11:07:39 -0700

    refactor

--- goatstone/notes/note.js
+++ goatstone/notes/note.js
@@ -1,10 +1,18 @@
 let debug = 11;
 
-const arr = [3, 45, 1, 34, 45, 5];
+const arr = [22, 3, 45, 1, 34, 45, 5];
 debug = arr;
 const x = () => {
-    for (let i = 0; i < 3; i += 1) {
-        arr.push(i);
+    for (let i = 0; i < arr.length; i += 1) {
+        let minI = i;
+        for (let j = i + 1; j < arr.length; j += 1) {
+            if (arr[minI] > arr[j]) {
+                minI = j;
+            }
+        }
+        const t = arr[i];
+        arr[i] = arr[minI];
+        arr[minI] = t;
     }
     return arr;
 };
